f18fcdfc93d0908b8c40e2f8a9ae484aea668369
Maken Hubbard <makhub26@outlook.com>
2018-02-27 21:19:04 -0600

    adding the excercises for for loops

--- arrays.js
+++ arrays.js
@@ -19,4 +19,36 @@ if (test1 === array1 ) {
 }
 
 
+var animals = ["fish", "dog", "cat"];
+var animalzElement = document.getElementById('animalz');
+for(var i=0; i<animals.length; i++) {
+// console.log("animal", animals[i])
+ animalzElement.innerHTML += '<h5>' + animals[i] + '</h5>';
+}
 
+
+//Challenge #2
+
+var book1 = 'catch 22';
+var array1 = book1.split(" ").pop();
+    console.log(array1);
+
+var book2 = 'Slaughterhouse 5';
+var array2 = book2.split(" ").pop();
+    console.log(array2);
+
+var book3 = 'Fahrenheit 451';
+var array3 = book2.split(" ").pop();
+    console.log(array3);
+
+var book4 = 'jaws 33';
+var bookSplit = book4.split("");
+var realNumz = [];
+for(var j=0; j<bookSplit.length; j++){
+    console.log('bookSplit', bookSplit[j]);
+    if((bookSplit[j]*1)){
+        realNumz.push(bookSplit[j]);
+        console.log('booksplit', bookSplit[j]);
+    }
+}
+console.log("answer",realNumz.join(""))
