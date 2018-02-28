69d1a0f3a62c8877c95e848d8041aa65e200c92a
Maken Hubbard <makhub26@outlook.com>
2018-02-27 19:47:09 -0600

    adding the exercise 1

---
+++ arrays.js
@@ -0,0 +1,22 @@
+console.log('arrays');
+
+//challenge #1
+
+var test1 = 'cat';
+var test2 = "racecar";
+var test3 = "bear";
+var test1 = "mom";
+
+
+var array1 = test1.split('');
+var reversed = array1.reverse();
+var array1 = reversed.join('');
+console.log(array1);
+if (test1 === array1 ) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
